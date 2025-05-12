3fe73f5d5bece8e179d22a202e7301efa346a4cf
mohamedf332 <mohamedfares.6778@gmail.com>
2025-05-12 23:55:11 +0300

    switch

--- main.cpp
+++ main.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
 #include "linked_list.h"
-
+#include "menu.h"
 
 int main() {
     linked_list list;
     
-    // Add sample employees
+    // sample employees
     std::cout << "Adding sample employees...\n";
     list.insert_employee("John", "Manager");
     list.insert_employee("Jane", "Developer");
-    list.insert_employee("Bob", "Designer");
-    list.display_employee();
-    list.delete_employee(1);
-    list.display_employee();
-    list.delete_employee_by_name("Jane");
-    list.display_employee();
-    list.search_employee(2);
-    list.search_employee_by_name("Bob");
+    list.insert_employee("Bob", "Designer"
+        
+    Menu menu(list);
+    menu.run();
+    
     return 0;
 }
